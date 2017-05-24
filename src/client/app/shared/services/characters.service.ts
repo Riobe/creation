b2b3fda772f77a43d17d319271ac12fe31c35e11
riobe <jeremy.pridemore@gmail.com>
2017-05-23 18:13:27 -0600

    BROKEN: Move home component, and add test file. Switch to promise from observable for http call.

--- src/client/app/shared/services/characters.service.ts
+++ src/client/app/shared/services/characters.service.ts
@@ -8,42 +8,21 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 export class CharactersService {
   constructor(private http: Http) { }
 
-  public getCharacter(id) {
+  public getCharacter(id): Promise<any> {
     const url = `/api/characters/${id}`;
     const options = new RequestOptions(new Headers({
       'Content-Type': 'application/json'
     }));
 
     return this.http.get(url).map((response: Response) => {
-      const event = response.json();
-      return event;
-    })
-    .catch(this.handleError);
+        const event = response.json();
+        return event;
+      })
+      .toPromise()
+      .catch(this.handleError);
   }
 
   private handleError(error: Response) {
-    return Observable.throw(error.statusText);
+    throw error.statusText;
   }
-  // public addVoter(eventId: number, session: ISession, voterName: string) {
-    // const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
-    // const options = new RequestOptions(new Headers({
-      // 'Content-Type': 'application/json'
-    // }));
-
-    // session.voters.push(voterName);
-    // return this.http.post(url, JSON.stringify({}), options)
-      // .catch(error => Observable.throw(error.statusText));
-  // }
-
-  // public deleteVoter(eventId: number, session: ISession, voterName: string) {
-    // const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
-
-    // session.voters = session.voters.filter(voter => voter !== voterName);
-    // return this.http.delete(url)
-      // .catch(error => Observable.throw(error.statusText));
-  // }
-
-  // public userHasVoted(session: ISession, voterName: string) {
-    // return session.voters.some(name => name === voterName);
-  // }
 }

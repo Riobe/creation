2c4d7c30ef0407288c45ddf2df44ec76182c2e5e
riobe <jeremy.pridemore@gmail.com>
2017-05-23 18:14:26 -0600

    Fixes to home.component after rename.

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,17 +1,17 @@
 'use strict';
 
 import { Component, OnInit } from '@angular/core';
-import { CharactersService } from './shared/services';
+import { CharactersService } from '../shared/services';
 
 @Component({
-  templateUrl: './home.template.pug'
+  templateUrl: './home.template.pug',
 })
 export class HomeComponent implements OnInit {
   public character;
   constructor(public charactersService: CharactersService) {}
 
   public ngOnInit() {
-    this.charactersService.getCharacter(1).subscribe(character => {
+    this.charactersService.getCharacter(1).then(character => {
       this.character = JSON.stringify(character, null, 2);
     });
   }

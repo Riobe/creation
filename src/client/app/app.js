574257aebd03a2fad5513735155633dfaa627c6c
riobe <jeremy.pridemore@gmail.com>
2017-09-15 15:58:03 -0600

    Now that we're linting correctly, put back in the jshint ignore line on Vue's constructor call.

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,8 +1,8 @@
 // To actually trigger Vue, it's implemented as a constructor called for it's
 // side effect. (Which is why we have jshint ignore the line)
-new Vue({ 
+new Vue({ // jshint ignore:line
   el: '#body',
   data: {
-    title: 'Hello Vue!!'
+    title: 'Hello Vue!!!'
   }
 });

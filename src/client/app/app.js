d70199e79598bd33972822e58e77019ff56ec09b
riobe <jeremy.pridemore@gmail.com>
2017-09-15 16:21:59 -0600

    Add npm debug to browser and add sample log.

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,3 +1,7 @@
+const debug = require('debug')('jeremypridemore-me:app');
+
+debug('Instantiating Vue.js');
+
 // To actually trigger Vue, it's implemented as a constructor called for it's
 // side effect. (Which is why we have jshint ignore the line)
 new Vue({ // jshint ignore:line

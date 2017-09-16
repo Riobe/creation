e88a32174047c19bcd5ec5e23b45eb3d2b3c2481
riobe <jeremy.pridemore@gmail.com>
2017-09-15 21:58:10 -0600

    Add some password checks (char count, password included, top 10k passwords)

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@
 const crypto = require('crypto'),
       pbkdf2 = require('pbkdf2'),
       mongoose = require('mongoose'),
+      topPasswords = require('../utility/top-10k-passwords'),
       log = require('debug')('jeremypridemore-me:models:user');
 
 log('Defining User model.');
@@ -14,24 +15,52 @@ const userSchema = mongoose.Schema({
   email: String
 });
 
-function hashPassword(password, salt) {
+function hash(password, salt) {
   return pbkdf2.pbkdf2Sync(password, salt, 10, 128, 'sha512').toString('hex');
 }
 
-userSchema.methods.rehashPassword = function() {
-  if (!this.password) { return; }
+userSchema.statics.validatePassword = function(password) {
+  const errors = [];
 
-  this.salt = crypto.randomBytes(32).toString('hex');
-  this.password = hashPassword(this.password, this.salt);
+  const characterCount = password
+    .split('')
+    .filter((character, i, array) => array.indexOf(character) === i)
+    .length;
+
+  if (characterCount < 5) {
+    errors.push('Have at least 5 unique characters in your password');
+  }
+
+  if (/password/i.test(password)) {
+    errors.push('Cannot have the word "password" in your password.');
+  }
+
+  if (topPasswords[password]) {
+    errors.push('You are trying to use one of the top 10,000 most common passwords. Please choose a better password.');
+  }
+
+  return errors;
+};
+
+userSchema.statics.hashPassword = function(user) {
+  if (!user.password) { return; }
+
+  user.salt = crypto.randomBytes(32).toString('hex');
+  user.password = hash(user.password, user.salt);
 };
 
 userSchema.methods.isPassword = function(password) {
   if (!password) { return false; }
   if (!this.salt || !this.password) { return false; }
 
-  return hashPassword(password, this.salt) === this.password;
+  return hash(password, this.salt) === this.password;
 };
 
 const User = mongoose.model('User', userSchema);
+
+log(User.validatePassword('aaaaaaaaaaa'));
+log(User.validatePassword('password123'));
+log(User.validatePassword('collect'));
+log(User.validatePassword('POSINDFpsnos8792!#$OMG?'));
 
 module.exports = User;

db03824f31fb56a2adccd52462a7c1bc2551f401
riobe <jeremy.pridemore@gmail.com>
2017-09-15 21:22:23 -0600

    Add bluebird to the project and setup mongoose to use it.

--- src/setup/mongoose.setup.js
+++ src/setup/mongoose.setup.js
@@ -1,16 +1,22 @@
 'use strict';
 
 const mongoose = require('mongoose'),
+      bluebird = require('bluebird'),
       log = require('debug')('jeremypridemore-me:setup:mongoose');
 
 log('Setting up mongoose.');
+
+// http://mongoosejs.com/docs/promises.html#plugging-in-your-own-promises-library
+mongoose.Promise = bluebird;
 
 const connectionUri = process.env.DATABASE_URI || 'mongodb://localhost/test';
 log(`Connecting to MongoDB at ${connectionUri}.`);
 
 mongoose.connect(connectionUri, {
   // http://mongoosejs.com/docs/connections.html#use-mongo-client
-  useMongoClient: true
+  useMongoClient: true,
+  // http://mongoosejs.com/docs/promises.html#promises-for-the-mongodb-driver
+  promiseLibrary: bluebird
 });
 
 log('Registering models with mongoose.');

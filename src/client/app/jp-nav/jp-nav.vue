4264636f0b8905cb535ec1b2fb795c839b810fb3
riobe <jeremy.pridemore@gmail.com>
2017-09-17 16:20:39 -0600

    Get example of subcomponents.

---
+++ src/client/app/jp-nav/jp-nav.vue
@@ -0,0 +1,3 @@
+<template lang="pug" src="./jp-nav.template.pug"></template>
+<script src="./jp-nav.script.js"></script>
+<style lang='scss' src="./jp-nav.styles.scss"></style>
--- src/client/app/jp-site/jp-site.script.js
+++ src/client/app/jp-site/jp-site.script.js
@@ -1,6 +1,11 @@
 'use strict';
 
+import jpNav from '../jp-nav/jp-nav.vue';
+
 export default {
+  components: {
+    jpNav
+  },
   data: function() {
     return {
       title: 'Hello Vue!!!'

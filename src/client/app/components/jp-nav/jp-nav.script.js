01f8c85aa3fa8e559c7f968f733fd68fb47fdfad
riobe <jeremy.pridemore@gmail.com>
2017-09-18 16:43:23 -0600

    Add login/register modals in basic state.

--- src/client/app/components/jp-nav/jp-nav.script.js
+++ src/client/app/components/jp-nav/jp-nav.script.js
@@ -2,12 +2,32 @@
 
 import jpModal from '../jp-modal/jp-modal.vue';
 
+const toastr = require('toastr');
+
 export default {
   data: function() {
     return {
+      registerForm: {
+        userName: undefined,
+        password: undefined,
+        email: undefined
+      },
+      loginForm: {
+        userName: undefined,
+        password: undefined
+      },
       loggingIn: true,
-      registering: false
+      registering: false,
+      user: undefined
     };
+  },
+  methods: {
+    login: function() {
+      this.user = {
+        userName: this.loginForm.userName
+      };
+      toastr.success('You are logged in as ' + this.user.userName, 'Success');
+    }
   },
   components: {
     jpModal
